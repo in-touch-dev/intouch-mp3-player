96a155da16d39b055491af5643c505d9e411653c
Dannish Hussain <dannish.hussain@intouchnetworks.com>
2019-09-09 12:19:30 +0100

    further updates

--- src/lib/components/Playlist.js
+++ src/lib/components/Playlist.js
@@ -6,146 +6,146 @@ import Player from "./Player";
 export default class Playlist extends React.Component {
 	constructor(props) {
 		super(props);
-	
+
 		this.state = {
-		  currentIndex: this.props.currentIndex || 0,
-		  activeTrack: this.props.tracks[this.props.currentIndex || 0],
-		  isMobile: false
+			currentIndex: this.props.currentIndex || 0,
+			activeTrack: this.props.tracks[this.props.currentIndex || 0],
+			isMobile: false
 		};
+	}
+
+	componentWillReceiveProps(newProps) {
+		this.setState({
+			activeTrack: newProps.tracks[newProps.currentIndex || 0],
+			currentIndex: newProps.currentIndex || 0
+		}, () => {
+			console.log('::Playlist this : ', this);
+		})
+	}
+
+	componentWillMount() {
+		let bp
+		if (this.props.opts) {
+			bp = this.props.opts.breakpoint ? (Math.abs(this.props.opts.breakpoint.maxWidth + (this.props.opts.offset && this.props.opts.offset.left ? this.props.opts.offset.left : 0) + (this.props.opts.offset && this.props.opts.offset.right ? this.props.opts.offset.right : 0))) : 768
+		} else {
+			bp = 768
 		}
-		
-		componentWillReceiveProps (newProps) {
-				this.setState({
-					activeTrack: newProps.tracks[this.props.currentIndex || 0],
-					currentIndex: this.props.currentIndex || 0
-				}, () => {
-					console.log('::Playlist this : ', this);
-				})
-		}
-	
-	  componentWillMount() {
-		  let bp
-		  if(this.props.opts) {
-		   bp = this.props.opts.breakpoint ? (Math.abs( this.props.opts.breakpoint.maxWidth + (this.props.opts.offset && this.props.opts.offset.left ? this.props.opts.offset.left : 0) + (this.props.opts.offset && this.props.opts.offset.right ? this.props.opts.offset.right : 0))) : 768
-		  } else {
-			  bp = 768
-		  }
-	
+
 		this.breakpoint = window.matchMedia(
-		  `(max-width: ${ bp }px)`
+			`(max-width: ${bp}px)`
 		);
 		const breakpointHandler = () => {
-		  this.setState({ isMobile: this.breakpoint.matches });
+			this.setState({ isMobile: this.breakpoint.matches });
 		};
-	
+
 		this.breakpoint.addListener(breakpointHandler);
 		breakpointHandler();
-		}
-	
-	  viewPlaylistBox = evt => {
+	}
+
+	viewPlaylistBox = evt => {
 		evt.preventDefault();
 		if (!this.state.viewPlaylist) this.setState({ viewPlaylist: true });
 		else this.setState({ viewPlaylist: false });
-	  };
-	
-	  playlistContent() {
+	};
+
+	playlistContent() {
 		const playlist = this.props.tracks.map((track, key) => {
-		  return (
-			<button
-			  className="mp3-player-playlist-track-button"
-			  key={key}
-			  onClick={() => this.setActiveTrack(track)}
-			>
-			  {track.name}
-			</button>
-		  );
+			return (
+				<button
+					className="mp3-player-playlist-track-button"
+					key={key}
+					onClick={() => this.setActiveTrack(track)}
+				>
+					{track.name}
+				</button>
+			);
 		});
 		return playlist;
-	  }
-	
-	  setActiveTrack = track => {
+	}
+
+	setActiveTrack = track => {
 		if (track.src === this.state.activeTrack) {
-		  return;
+			return;
 		}
 		this.setState({ activeTrack: track, showPlaylistBody: false });
-	  };
-	
-	  playlistClickHandler(evt, overide) {
+	};
+
+	playlistClickHandler(evt, overide) {
 		this.setState({
-		  showPlaylistBody:
-			typeof overide !== "undefined" ? overide : !this.state.showPlaylistBody
+			showPlaylistBody:
+				typeof overide !== "undefined" ? overide : !this.state.showPlaylistBody
 		});
-	  }
-	
-	  playlistBody() {
+	}
+
+	playlistBody() {
 		if (this.state.hidePlaylist) {
-		  return;
+			return;
 		}
 		const showPlaylist = this.state.showPlaylistBody ? "playlist" : "";
-	  const isMobile = this.state.isMobile ? 'is-mobile' : '';
-	
-		const styleOffsetOverides = Object.assign( {}, { left : 0, right : 0 }, this.props.opts && this.props.opts.offset );
-		styleOffsetOverides.left = `${ styleOffsetOverides.left }px`;
-		styleOffsetOverides.right = `${ styleOffsetOverides.right }px`;
-	
+		const isMobile = this.state.isMobile ? 'is-mobile' : '';
+
+		const styleOffsetOverides = Object.assign({}, { left: 0, right: 0 }, this.props.opts && this.props.opts.offset);
+		styleOffsetOverides.left = `${styleOffsetOverides.left}px`;
+		styleOffsetOverides.right = `${styleOffsetOverides.right}px`;
+
 		return (
-		  <div className={`mp3-player-playlist-container ${showPlaylist} ${isMobile}`} style={ this.state.isMobile ? styleOffsetOverides : {} }>
-			<div className="mp3-player-playlist-header">
-			  <button
-				className="mp3-player-playlist-close"
-				onClick={evt => this.playlistClickHandler(evt)}
-			  >
-				<Icon iconName="close" fill={"white"} />
-			  </button>
+			<div className={`mp3-player-playlist-container ${showPlaylist} ${isMobile}`} style={this.state.isMobile ? styleOffsetOverides : {}}>
+				<div className="mp3-player-playlist-header">
+					<button
+						className="mp3-player-playlist-close"
+						onClick={evt => this.playlistClickHandler(evt)}
+					>
+						<Icon iconName="close" fill={"white"} />
+					</button>
+				</div>
+				<div className="mp3-player-playlist-content">
+					{this.playlistContent()}
+				</div>
 			</div>
-			<div className="mp3-player-playlist-content">
-			  {this.playlistContent()}
-			</div>
-		  </div>
 		);
-	  }
-	
-	  skipHandler(evt, type) {
+	}
+
+	skipHandler(evt, type) {
 		const newIndex =
-		  type === "next"
-			? this.state.currentIndex + 1
-			: this.state.currentIndex - 1;
-	
+			type === "next"
+				? this.state.currentIndex + 1
+				: this.state.currentIndex - 1;
+
 		if (typeof this.props.tracks[newIndex] !== "undefined") {
-		  this.setState({
-			currentIndex: newIndex,
-			activeTrack: this.props.tracks[newIndex]
-		  });
-		  return;
+			this.setState({
+				currentIndex: newIndex,
+				activeTrack: this.props.tracks[newIndex]
+			});
+			return;
 		}
-	
+
 		console.error("no song to play!");
-	  }
-	
-	  togglePlaylist(condition) {
+	}
+
+	togglePlaylist(condition) {
 		const state = { hidePlaylist: condition };
-	
+
 		if (condition) {
-		  state.showPlaylistBody = false;
+			state.showPlaylistBody = false;
 		}
 		this.setState(state);
-	  }
-	
-	  render() {
+	}
+
+	render() {
 		return (
-		  <div className={ this.state.isMobile ? "playlist-wrap is-mobile" : "playlist-wrap" }>
-			<Player
-			  activeTrack={this.state.activeTrack}
-			  hasPlaylist={true}
-			  playlistClickHandler={evt => this.playlistClickHandler(evt)}
-			  skipHandler={(evt, type) => this.skipHandler(evt, type)}
-			  togglePlaylist={condition => this.togglePlaylist(condition)}
-			  isMobile={this.state.isMobile}
-				opts={ this.props.opts }
-				onClose={this.props.onClose}
-			/>
-			{this.playlistBody()}
-		  </div>
+			<div className={this.state.isMobile ? "playlist-wrap is-mobile" : "playlist-wrap"}>
+				<Player
+					activeTrack={this.state.activeTrack}
+					hasPlaylist={true}
+					playlistClickHandler={evt => this.playlistClickHandler(evt)}
+					skipHandler={(evt, type) => this.skipHandler(evt, type)}
+					togglePlaylist={condition => this.togglePlaylist(condition)}
+					isMobile={this.state.isMobile}
+					opts={this.props.opts}
+					onClose={this.props.onClose}
+				/>
+				{this.playlistBody()}
+			</div>
 		);
-	  }
+	}
 }
